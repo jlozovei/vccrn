2ee64bbb025ea2f2d19ef950383b643ef045430d
jlozovei <jlozovei@gmail.com>
2023-02-15 13:56:22 -0300

    feat: create tokens package

---
+++ packages/tokens/colors.js
@@ -0,0 +1,23 @@
+const baseColors = {
+  white: '#fff',
+  black: '#000',
+  gray: '#bdc3c7'
+};
+
+const brandColors = {
+  primary: '#e56048',
+  secondary: '#0a142f'
+};
+
+const fontColors = {
+  title: brandColors.primary,
+  text: baseColors.black
+};
+
+const colors = {
+  base: baseColors,
+  brand: brandColors,
+  font: fontColors
+};
+
+export { colors };
