4f1a8b85bc2966c10fc2a5526043891a0b5a0eea
jlozovei <jlozovei@gmail.com>
2023-02-15 22:02:55 -0300

    feat: create Header compoonent

---
+++ apps/web/src/components/Header/index.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+import { StyledContainer } from '@/styled';
+import { Logo } from '@/components';
+
+import {
+  StyledHeader,
+  StyledMenu,
+  StyledMenuList,
+  StyledMenuListItem,
+  StyledMenuLink,
+  StyledHamburger
+} from './styled';
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleResponsibleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <StyledHeader>
+      <StyledContainer>
+        <Logo width="130" data-testid="header-logo" />
+
+        <StyledHamburger
+          onClick={() => handleResponsibleMenu()}
+          open={menuOpen}
+          data-testid="header-hamburger"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </StyledHamburger>
+
+        <StyledMenu data-testid="header-menu" open={menuOpen} aria-expanded={menuOpen}>
+          <StyledMenuList>
+            <StyledMenuListItem>
+              <StyledMenuLink>Como funciona</StyledMenuLink>
+            </StyledMenuListItem>
+            <StyledMenuListItem>
+              <StyledMenuLink>Para você</StyledMenuLink>
+            </StyledMenuListItem>
+            <StyledMenuListItem>
+              <StyledMenuLink>Para psicólogos</StyledMenuLink>
+            </StyledMenuListItem>
+            <StyledMenuListItem>
+              <StyledMenuLink>Para empresas</StyledMenuLink>
+            </StyledMenuListItem>
+            <StyledMenuListItem>
+              <StyledMenuLink>Blog</StyledMenuLink>
+            </StyledMenuListItem>
+          </StyledMenuList>
+        </StyledMenu>
+      </StyledContainer>
+    </StyledHeader>
+  );
+};
+
+export { Header };
