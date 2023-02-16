6cff3e9ac7c11732270ce72c8c45941b7e395c36
jlozovei <jlozovei@gmail.com>
2023-02-16 12:44:56 -0300

    feat: add msw for react app

---
+++ apps/web/src/mocks/handlers.js
@@ -0,0 +1,26 @@
+import { rest } from 'msw';
+
+const user = {
+  id: 'f79e82e8-c34a-4dc7-a49e-9fadc0979fda',
+  firstName: 'John',
+  lastName: 'Doe',
+  type: 'pacient'
+};
+
+const psychologist = {
+  id: '9146d560-ae09-11ed-afa1-0242ac120002',
+  firstName: 'Alex',
+  lastName: 'Doe',
+  type: 'psychologist'
+};
+
+const handlers = [
+  rest.post('/login', (req, res, ctx) => {
+    const { email, document, password } = req.body;
+    const profile = document ? psychologist : user;
+
+    return res(ctx.json(profile));
+  })
+];
+
+export { handlers };
