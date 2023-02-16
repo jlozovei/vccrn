31c83a2f97fe7089d3934b14c4b2f7637a38e636
jlozovei <jlozovei@gmail.com>
2023-02-16 11:11:36 -0300

    test: update FormField test cases

--- apps/web/src/components/Form/Field/Field.test.jsx
+++ apps/web/src/components/Form/Field/Field.test.jsx
@@ -5,7 +5,15 @@ import { FormField } from '@/components';
 
 describe('components/FormField', () => {
   it('Should render correctly', async () => {
-    render(<FormField label="Email" name="email" placeholder="Insira seu email" type="text" />);
+    render(
+      <FormField
+        label="Email"
+        name="email"
+        placeholder="Insira seu email"
+        type="text"
+        required={false}
+      />
+    );
 
     const input = screen.getByLabelText('Email');
     const email = 'test@email.com';
@@ -14,5 +22,23 @@ describe('components/FormField', () => {
 
     await userEvent.type(input, email);
     expect(input).toHaveValue(email);
+  });
+
+  it('Should update label text and display error', () => {
+    render(
+      <FormField
+        label="Email"
+        name="email"
+        placeholder="Insira seu email"
+        type="text"
+        required={true}
+        error="Campo obrigatório"
+      />
+    );
+
+    const input = screen.getByLabelText('Email*');
+    expect(input).toBeInTheDocument();
+
+    expect(screen.getByText('Campo obrigatório')).toBeInTheDocument();
   });
 });
--- apps/web/src/components/Form/Field/index.jsx
+++ apps/web/src/components/Form/Field/index.jsx
@@ -4,14 +4,14 @@ import { Label, Input } from '../';
 import { StyledField, StyledError } from './styled';
 
 const Field = React.forwardRef(
-  ({ type, label, name, placeholder, errors, required, ...props }, ref) => {
+  ({ type, label, name, placeholder, error, required, ...props }, ref) => {
     const labelText = `${label}${required ? '*' : ''}`;
 
     return (
       <StyledField>
         <Label htmlFor={name} text={labelText} />
         <Input id={name} name={name} type={type} placeholder={placeholder} ref={ref} {...props} />
-        {errors && <StyledError>{errors}</StyledError>}
+        {error && <StyledError>{error}</StyledError>}
       </StyledField>
     );
   }
