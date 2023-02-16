37ca9a1a43ec5c1be5eb4b7e7ab686a519c8e362
jlozovei <jlozovei@gmail.com>
2023-02-15 22:01:37 -0300

    feat: add sizes tokens

--- packages/tokens/sizes.js
+++ packages/tokens/sizes.js
@@ -1,3 +1,20 @@
-const sizes = {};
+const viewports = {
+  mobile: 560,
+  tablet: 768,
+  medium: 992,
+  desktop: 1024
+};
+
+const grid = {
+  small: 320,
+  medium: 640,
+  large: 980,
+  full: 1360
+};
+
+const sizes = {
+  viewports,
+  grid
+};
 
 export { sizes };
