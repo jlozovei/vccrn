0483eb305bb9d10635d8c895af304590d7cdfc50
jlozovei <jlozovei@gmail.com>
2023-02-17 13:46:32 -0300

    feat: create strings package

---
+++ packages/strings/index.js
@@ -0,0 +1,32 @@
+const errors = {
+  required: 'Campo obrigatório.',
+  password: 'Senha muito curta.',
+  document: 'Documento inválido.',
+  email: 'Email inválido.'
+};
+
+const labels = {
+  email: 'Email',
+  document: 'CFP/CNPJ',
+  password: 'Senha'
+};
+
+const placeholders = {
+  email: 'Digite seu email',
+  password: 'Digite sua senha',
+  document: 'Digite seu documento'
+};
+
+// const regex = {
+//   document:
+//     /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/g
+// };
+
+const strings = {
+  errors,
+  labels,
+  placeholders
+  // regex
+};
+
+export { strings };
