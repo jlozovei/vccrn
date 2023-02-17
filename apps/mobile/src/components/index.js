2f79acc9f37089d48150ae85569ca810b04f5d1d
jlozovei <jlozovei@gmail.com>
2023-02-17 15:06:01 -0300

    feat: create component indexes

---
+++ apps/mobile/src/components/index.js
@@ -0,0 +1,6 @@
+export { Button } from './Button';
+export { Footer } from './Footer';
+export { Header } from './Header';
+export { Logo } from './Logo';
+
+export { TextField, Submit } from './Form';
