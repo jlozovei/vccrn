14394642b4e6907275b0ba7e57aa6621c1d71f2c
jlozovei <jlozovei@gmail.com>
2023-02-17 15:14:15 -0300

    feat: update App main file

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
+
+import { Header, Footer } from '@/components';
+import { LoginForm } from '@/containers';
+import { server } from '@/mocks';
+
+server();
+
+const styles = StyleSheet.create({
+  appWrapper: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
+    backgroundColor: '#fff'
+  }
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appWrapper}>
+      <ScrollView>
+        <Header />
+
+        <LoginForm />
+
+        <Footer />
+
+        <StatusBar style="auto" />
+      </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
